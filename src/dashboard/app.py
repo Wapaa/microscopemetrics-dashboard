999881d100cca5727cd4a81b258d0bc3e6c851c7
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-01-26 16:26:19 +0100

    add Field of illumniation interface

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,30 +1,72 @@
 import dash
 import dash_mantine_components as dmc
-from dash import Dash, dcc, html
+from dash import Dash, html
+from dash_iconify import DashIconify
 
-app = Dash(__name__, use_pages=True)
+external_stylesheets = [dmc.theme.DEFAULT_COLORS]
+
+
+def get_icon(icon):
+    return DashIconify(icon=icon, height=24)
+
+
+app = Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True)
+
 
 app.layout = html.Div(
     [
-        dmc.Navbar(
-            p="md",  # providing medium padding all side
-            fixed=False,  # Setting fixed to false
-            width={"base": 300},  # Initial size of navbar ie. 300px
-            hidden=True,  # we want to hide for smaller screen
-            hiddenBreakpoint="md",  # after past medium size navbar will be hidden.
-            height="100vh",  # providing height of navbar
-            id="sidebar",
-            children=[
-                dmc.Anchor("Link1", href="/"),
-                dmc.Anchor("Link2", href="testpage"),
-                dmc.Anchor("Link3", href="/"),
-                dmc.Anchor("Link4", href="/"),
+        html.Div(
+            [
+                dmc.Center(dmc.Image(src="/assets/images/logo.png", alt="logo", width=200)),
+                dmc.Center(
+                    dmc.Text(
+                        "Microscope Metrics Dashboard",
+                        color="#189A35",
+                        weight=700,
+                        size="lg",
+                        style={"margin-bottom": "20px"},
+                    )
+                ),
+                dmc.NavLink(
+                    label="Dashboard",
+                    icon=get_icon(icon="material-symbols:dashboard-outline"),
+                    href="/testpage",
+                ),
+                dmc.NavLink(
+                    label="Unnamed", icon=get_icon(icon="tabler:gauge"), href="/underconstruction"
+                ),
+                dmc.NavLink(
+                    label="Unnamed",
+                    icon=get_icon(icon="bi:house-door-fill"),
+                    href="/underconstruction",
+                ),
+                dmc.NavLink(
+                    label="Unnamed",
+                    icon=get_icon(icon="tabler:gauge"),
+                    href="/underconstruction",
+                ),
             ],
+            style={
+                "width": "18%",
+                "background": "white",
+                "height": "100vh",
+                "display": "inline-block",
+                "border-right": "solid 1px #e0e0e0",
+            },
         ),
-        dash.page_container,
-    ]
+        html.Div(
+            [dash.page_container],
+            style={
+                "width": "79%",
+                "height": "100vh",
+                "margin-left": "10px",
+                "display": "inline-block",
+                "background": "white",
+            },
+        ),
+    ],
+    style={"display": "flex", "justify-content": "space-between", "margin-bottom": "20px"},
 )
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/dashboard/pages/testPage.py
+++ src/dashboard/pages/testPage.py
@@ -1,11 +1,102 @@
 import dash
-from dash import html
+import dash_mantine_components as dmc
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from dash import dash_table, dcc, html
+from dash_iconify import DashIconify
+from linkml_runtime.loaders import yaml_loader
+from microscopemetrics_schema.datamodel.microscopemetrics_schema import (
+    FieldIlluminationDataset,
+)
+from PIL import Image
+
+obj = yaml_loader.load(
+    "/home/wapaa/projects/microscopemetrics-dashboard/src/data/examples/FieldIlluminationDataset-001.yaml",
+    target_class=FieldIlluminationDataset,
+)
+
+
+def get_intensity_profiles(var: FieldIlluminationDataset.output) -> pd.DataFrame:
+    data_dict = var.intensity_profiles.columns
+    dfs = [pd.DataFrame({key: value.values}) for key, value in data_dict.items()]
+    df = pd.concat(dfs, axis=1)
+    return df
+
+
+def get_intensity_map_data(var: FieldIlluminationDataset.output) -> Image:
+    list_ima = var.intensity_map.data
+    x = var.intensity_map.shape_x
+    y = var.intensity_map.shape_y
+    ima = np.array(list_ima)
+    pil_ima = Image.fromarray(ima.reshape([x, y]))
+    return pil_ima
+
+
+def get_key_values(var: FieldIlluminationDataset.output) -> pd.DataFrame:
+    data_dict = var.key_values.__dict__
+    data_dict = {
+        key: value[0] if isinstance(value, list) and value else value
+        for key, value in data_dict.items()
+    }
+    data_list = list(data_dict.items())
+    df = pd.DataFrame(data_list, columns=["Key", "Value"])
+    df.Key = df.Key.str.replace("_", " ").str.title()
+    return df
+
+
+data = get_key_values(obj.output)
+data_IP = get_intensity_profiles(obj.output)
+ima = get_intensity_map_data(obj.output)
 
 dash.register_page(__name__)
 
-layout = html.Div(
+layout = dmc.Container(
     [
-        html.H1("This is our test page"),
-        html.Div("This is our test page content."),
-    ]
+        dmc.Center(
+            dmc.Text(
+                "Field Of Illumination Analysis",
+                color="#189A35",
+                mb=30,
+                style={"margin-top": "20px", "fontSize": 40},
+            )
+        ),
+        dmc.Grid(
+            [
+                dmc.Col(
+                    [
+                        dmc.Title("Intensity Map", color="#189A35", size="h3", mb=10),
+                        dcc.Graph(
+                            figure=px.imshow(ima, zmin=0.0, zmax=1.0, color_continuous_scale="hot")
+                        ),
+                    ],
+                    span=6,
+                ),
+                dmc.Col(
+                    [
+                        dmc.Title("Key Values", color="#189A35", size="h3", mb=10),
+                        dash_table.DataTable(data.to_dict("records"), page_size=10),
+                    ],
+                    span=6,
+                ),
+            ]
+        ),
+        dmc.Grid(
+            [
+                dmc.Col(
+                    [
+                        dmc.Title("Intensity Profiles", color="#189A35", size="h3", mb=10),
+                        dash_table.DataTable(
+                            data_IP.to_dict("records"),
+                            page_size=10,
+                            style_table={"overflowX": "auto"},
+                        ),
+                    ],
+                    span=6,
+                ),
+                dmc.Col([dcc.Graph(figure=px.line(data_IP))], span=6),
+            ]
+        ),
+    ],
+    fluid=True,
 )
